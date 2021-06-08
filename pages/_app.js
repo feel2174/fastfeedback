68a9558233840e6ca513dfdce8fe8c0c485a28c0
Young Joo Kim <37773812+feel2174@users.noreply.github.com>
2021-06-08 22:53:42 +0900

    feat: connected firestore (#2)
    * feat: setting lint, prettier
    
    * fix: setting husky
    
    * fix: stable husky
    
    * feat: added Chakra-ui, firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { AuthProvider } from '@/lib/auth';
+import { Global, css } from '@emotion/react';
+import theme from '@/styles/theme';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
