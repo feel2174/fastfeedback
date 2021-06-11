1fb376cf7db9f0966053b78aa7d4ea0e010737ae
Young Joo Kim <feel2174@gmail.com>
2021-06-11 15:55:17 +0900

    feat: Added Dashboard, modify lint setting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    parser: 'babel-eslint',
+    extends: [
+        'plugin:prettier/recommended',
+        'plugin:react/recommended',
+    ],
+    plugins: ['react', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 6,
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'react/no-children-prop': 'off',
+    },
+    ignorePatterns: [
+        'node_modules',
+        'coverage',
+        'build',
+        'dist',
+        '.*',
+        '*.config.{js,ts}',
+    ],
+};
---
+++ pages/dashboard.js
@@ -0,0 +1,11 @@
+import DashBoardShell from '@/components/DashBoardShell';
+
+const DashBoard = () => {
+  return (
+    <>
+      <DashBoardShell />
+    </>
+  );
+};
+
+export default DashBoard;
