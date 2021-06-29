37689e69d898599648041f65b74517d3c3fe6962
Young Joo Kim <feel2174@gmail.com>
2021-06-29 21:49:20 +0900

    feat: added feedback form

---
+++ pages/p/[siteId].js
@@ -0,0 +1,57 @@
+import Feedback from '@/components/Feedback';
+import { getAllFeedback, getAllSites } from '@/lib/db-admin';
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+
+export async function getStaticProps(context) {
+  const siteId = context.params.siteId;
+  const feedback = await getAllFeedback(siteId);
+  return {
+    props: {
+      initialFeedback: feedback,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const sites = await getAllSites();
+  const paths = sites.map((site) => ({
+    params: {
+      siteId: site.id.toString(),
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+const SiteFeedback = ({ initialFeedback }) => {
+  console.log(initialFeedback[0]);
+  const onSubmit = () => {
+    console.log('Hello!');
+  };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      width="full"
+      maxWidth="700px"
+      margin="0 auto"
+    >
+      <Box as="form" onSubmit={onSubmit}>
+        <FormControl my={8} id="email">
+          <FormLabel htmlFor="comment">Comment</FormLabel>
+          <Input type="comment" id="comment" />
+          <Button mt={2} type="submit" fontWeight="medium">
+            Add Comment
+          </Button>
+        </FormControl>
+      </Box>
+      {initialFeedback.map((feedback) => (
+        <Feedback key={feedback.id} {...feedback} />
+      ))}
+    </Box>
+  );
+};
+
+export default SiteFeedback;
