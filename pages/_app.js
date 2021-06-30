637a0f9c59566390e5cf5677fb0f713d585d75eb
Young Joo Kim <37773812+feel2174@users.noreply.github.com>
2021-06-30 10:39:39 +0900

    feat: modified og image url (#9)
    * feat: setting lint, prettier
    
    * fix: setting husky
    
    * fix: stable husky
    
    * feat: added Chakra-ui, firestore
    
    * feat: Added favicon, manifest
    
    * test:
    
    * feat: Added Dashboard, modify lint setting
    
    * fix: build error
    
    * feat: added dashboard, add site function
    
    * feat: added site name,url get API
    
    * feat: added useSWR
    
    * feat: added feedback form
    
    * deleted log
    
    * feat: added meta tag info
    
    * feat: modified og image

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,30 @@ import { Global, css } from '@emotion/react';
 import theme from '@/styles/theme';
 import Head from 'next/head';
 
+const SITE_NAME = 'Fast Feedback';
+const SITE_TITLE = 'Fast Feedback';
+const SITE_DESCRIPTION = 'Fast Feedback';
+const SITE_IMAGE = '/images/og.png';
+
 const GlobalStyle = ({ children }) => {
   return (
     <>
       <Head>
         <title>Fast Feedback</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="canonical" href="https://fastfeedback-orcin.vercel.app/" />
+        <meta name="description" content={SITE_DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={SITE_NAME} />
+        <meta property="og:title" content={SITE_TITLE} />
+        <meta property="og:description" content={SITE_DESCRIPTION} />
+        <meta property="og:image" content={SITE_IMAGE} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={SITE_NAME} />
+        <meta name="twitter:title" content={SITE_TITLE} />
+        <meta name="twitter:description" content={SITE_DESCRIPTION} />
+        <meta property="twitter:image" content={SITE_IMAGE} />
       </Head>
       <CSSReset />
       <Global
