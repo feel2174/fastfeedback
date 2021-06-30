134d6a7b5113c1caea6e3e307f1acee0e138129e
Young Joo Kim <feel2174@gmail.com>
2021-06-30 10:25:10 +0900

    feat: added meta tag info

--- components/DashboardShell.js
+++ components/DashboardShell.js
@@ -37,8 +37,8 @@ const DashBoardShell = ({ children }) => {
               d="M19.557.113C11.34.32 9.117 8.757 9.03 12.95c1.643-2.67 4.62-3.08 6.931-3.08 2.825.085 10.27.205 17.458 0C40.61 9.663 44.802 3.28 46 .112c-5.391-.085-18.228-.205-26.443 0zM14.422 14.234C3.332 14.234-.468 24.76.045 31.948c3.594-6.418 7.617-7.53 9.243-7.445h6.675c5.956 0 11.039-6.846 12.836-10.27H14.422z"
             />
           </Icon>
-          <Link>Feedback</Link>
-          <Link>Sites</Link>
+          <Link href="/">Feedback</Link>
+          <Link href="/dashboard">Sites</Link>
         </Stack>
         <Flex justifyContent="space-between" alignItems="center">
           <Link mr={4}>Log Out</Link>
