5a71afb071b8b597b4b0e2eab3051cadcb404a5f
Young Joo Kim <feel2174@gmail.com>
2021-07-02 09:21:56 +0900

    feat: added feedback page

--- components/Feedback.js
+++ components/Feedback.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Box, Heading, Text, Divider } from '@chakra-ui/react';
 import { format, parseISO } from 'date-fns';
 
-const Feedback = ({ Author, text, createdAt }) => {
+const Feedback = ({ author, text, createdAt }) => {
   return (
     <Box borderRadius={4} maxWidth="700px" w="full">
       <Heading size="sm" as="h3" mb={0} color="gray.900" fontWeight="medium">
-        {Author}
+        {author}
       </Heading>
       <Text color="gray.500" mb={4} fontSize="xs">
         {format(parseISO(createdAt), 'PPpp')}
